693fc1d36b0db398f752dc6323ec383ede09d024
ChauveSouris <desseobscurite@gmail.com>
2023-06-06 00:39:27 +0300

    Added InfoPerson

---
+++ ContactList/ViewControlers/InfoContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoContactViewController.swift
+//  ContactList
+//
+//  Created by Елизавета Медведева on 05.06.2023.
+//
+
+import UIKit
+
+class InfoContactViewController: UIViewController {
+    
+    @IBOutlet var numberPerson: UILabel!
+    @IBOutlet var emailPerson: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      
+    }
+
+
+}
