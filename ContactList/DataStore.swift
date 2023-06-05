f437b33e1d20b272f83ed55c0fb973fce0f4c51a
ChauveSouris <desseobscurite@gmail.com>
2023-06-06 00:38:25 +0300

    Added DataStore

--- ContactList/DataStore.swift
+++ ContactList/DataStore.swift
@@ -5,7 +5,6 @@
 //  Created by Елизавета Медведева on 05.06.2023.
 //
 
-
 class DataStore {
     
     let name: Set<String>
@@ -13,7 +12,12 @@ class DataStore {
     let number: Set<String>
     let email: Set<String>
     
-    init(name: Set<String>, surname: Set<String>, number: Set<String>, email: Set<String>) {
+    init(
+        name: Set<String>,
+        surname: Set<String>,
+        number: Set<String>,
+        email: Set<String>
+    ) {
         self.name = name
         self.surname = surname
         self.number = number
@@ -21,3 +25,10 @@ class DataStore {
         
     }
 }
+
+let persons = DataStore(
+    name: ["Tom", "Cook", "Anna", "Yen", "Sem"],
+    surname: ["Green", "None", "Feel", "Dunt", "Bim"],
+    number: ["+1 111 11", "+1 222 22", "+1 333 33", "+1 444 44", "+1 555 55"],
+    email: ["1111@email.com", "2222@email.com", "3333@email.com", "4444@email.com", "5555@email.com"]
+)
