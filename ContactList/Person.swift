1f524ffaf7a9e2a74aa5268ae0bb65f5fb6c30a7
ChauveSouris <desseobscurite@gmail.com>
2023-06-06 00:39:03 +0300

    Added Person

--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -5,7 +5,27 @@
 //  Created by Елизавета Медведева on 05.06.2023.
 //
 
+private var dataStore: DataStore!
 
 struct Person {
     
+    let name: Set<String>
+    let surname: Set<String>
+    let number: Set<String>
+    let email: Set<String>
+    
+    var fullname: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPerson() -> [Person] {
+        [
+            Person(
+                name: dataStore.name,
+                surname: dataStore.surname,
+                number: dataStore.number,
+                email: dataStore.email
+            )
+        ]
+    }
 }
