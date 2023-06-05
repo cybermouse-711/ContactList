a99a508bf7d198c513ab94a68f919dad1942cb89
ChauveSouris <desseobscurite@gmail.com>
2023-06-05 22:59:19 +0300

    Create VC Info about Person

---
+++ ContactList/InfoContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InfoContactViewController.swift
+//  ContactList
+//
+//  Created by Елизавета Медведева on 05.06.2023.
+//
+
+import UIKit
+
+class InfoContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
