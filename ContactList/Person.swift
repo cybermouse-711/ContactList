dbdf1c8ba6f76297c63436da5dc0427fea427849
ChauveSouris <desseobscurite@gmail.com>
2023-06-07 22:55:00 +0300

    Added func for Model

--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -5,7 +5,7 @@
 //  Created by Елизавета Медведева on 05.06.2023.
 //
 
-private var dataStore: DataStore!
+import UIKit
 
 struct Person {
     
@@ -15,17 +15,56 @@ struct Person {
     let email: String
     
     var fullName: String {
-        "\(name) \(surname)"
+        "\(name) \(surname)" 
     }
     
-    static func getPerson() -> [Person] {
-        [
-            Person(
-                name: dataStore.name.randomElement() ?? "not name",
-                surname: dataStore.surname.randomElement() ?? "not surname",
-                number: dataStore.number.randomElement() ?? "not number",
-                email: dataStore.email.randomElement() ?? "not email"
-            )
-        ]
-    }
+    static func getPersons() -> [Person] {
+            let dataStore = DataStore()
+
+            var names = dataStore.name
+            var surnames = dataStore.surname
+            var numbers = dataStore.number
+            var emails = dataStore.email
+
+            var persons = [Person]()
+            
+            while names.count > 0 {
+
+                var name = ""
+                var surname = ""
+                var number = ""
+                var email = ""
+                
+                if let index = names.indices.randomElement() {
+                    name = names[index]
+                    names.remove(at: index)
+                }
+                
+                if let index = surnames.indices.randomElement() {
+                    surname = surnames[index]
+                    surnames.remove(at: index)
+                }
+                
+                if let index = numbers.indices.randomElement() {
+                    number = numbers[index]
+                    numbers.remove(at: index)
+                }
+                
+                if let index = emails.indices.randomElement() {
+                    email = emails[index]
+                    emails.remove(at: index)
+                }
+
+                persons.append(
+                     Person(
+                        name: name,
+                        surname: surname,
+                        number: number,
+                        email: email
+                    )
+                )
+            }
+            return persons
+        }
+    
 }
